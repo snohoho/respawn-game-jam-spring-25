93d79f8e21c4c64ee9c82bf5a29dfe4c6d028f32
Jesus De La Rosa <budderlod@gmail.com>
2025-02-21 23:52:57 -0500

    e

--- Assets/Scripts/ViewersManager.cs
+++ Assets/Scripts/ViewersManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -9,35 +10,52 @@ public class ViewersManager : MonoBehaviour
     [SerializeField] CodingMicrogame codingMicrogame;
     [SerializeField] DiscordMicrogame discordMicrogame;
     //[SerializeField] DonoMicrogame donoMicrogame;
-    //[SerializeField] ChatMicrogame chatMicrogame;
+    [SerializeField] ChatMicrogame chatMicrogame;
+
     private int viewers;
-    private float sinceStartTimer;
+    [SerializeField] TextMeshProUGUI viewerCountTxt;
     private float viewerLossTimer;
+    private float multIncreaseTimer;
     private float viewerLossMult;
+    [SerializeField] TextMeshProUGUI uptimeTxt;
 
     void Start()
     {
-        viewers = 1000;
-        sinceStartTimer = 0f;
-        viewerLossMult = 1f;
+        viewers = Random.Range(990,1011);
+        viewerLossMult = Random.Range(0.95f, 1.05f);
     }
 
     void Update()
     {
-        
+        viewerCountTxt.text = "Viewers: " + viewers.ToString();
+
+        int time = (int)Time.timeSinceLevelLoad;
+        int ss = time%60;
+        int mm = time/60%60;
+        string timeTxt = string.Format("{0:00}:{1:00}",mm,ss);
+
+        uptimeTxt.text = "Uptime: " + timeTxt;
     }
 
     void FixedUpdate() {
-        sinceStartTimer = Time.timeSinceLevelLoad;
         viewerLossTimer += Time.deltaTime;
+        multIncreaseTimer += Time.deltaTime;
 
-        //every 30s increase the multiplier for lost viewers
-        if(Mathf.Round(sinceStartTimer % 30) == 0) {
-            viewerLossMult += 0.25f;
+        //every 15s increase the multiplier for lost viewers
+        if(multIncreaseTimer >= 15) {
+            viewerLossMult += Random.Range(0.15f,0.25f);
+            multIncreaseTimer = 0f;
         }
 
+        //lose viewers every second
         if(viewerLossTimer > 1f) {
             viewers -= (int)Mathf.Round(10.0f * viewerLossMult);
+            viewerLossTimer = 0f;
+        }
+
+        if(chatMicrogame.chatClicked == true) {
+            chatMicrogame.chatClicked = false;
+            viewers += 100;
         }
     }
 }
